9dbd605523b6786b0c015ebbfddba7cf70f82f90
MichaelCND <81520034+MichaelCND@users.noreply.github.com>
2023-05-23 23:14:36 +0300

    1
    add creation file

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import os
 
 def create_file():
-    qq =2
-    print(qq)
+    print('enter how much itens in your plan \n')
+    hm_itm = int(input())
+    new_file = open("D:/files_for_python/timetable.txt", "x")
+    for x in range(hm_itm):
+        print('enter '+ str(x+1) +' item \n')
+        what_to_write = input()
+        new_file.write(what_to_write + '\n')
+
 def load_file():
     q=1
     print(q)
 
-print('hi, click "c" if you want to create new plan, or click "s" if you want to see existed ')
+print('hi, click "c" if you want to create new plan, or click "s" if you want to see existed \n ')
 
 first_check = input()
 
