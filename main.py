f0c57ea7b0b3032abf1f53adeac6500e65cd8d4b
MichaelCND <81520034+MichaelCND@users.noreply.github.com>
2023-05-24 18:44:24 +0300

    1
    add selection of file name

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import os
 
 def create_file():
+    print('what name do you want to give the timetable \n')
+    file_name = input()
     print('enter how much itens in your plan \n')
     hm_itm = int(input())
-    new_file = open("D:/files_for_python/timetable.txt", "x")
+    new_file = open("D:/files_for_python/" + file_name +".txt", "x")
     for x in range(hm_itm):
         print('enter '+ str(x+1) +' item \n')
         what_to_write = input()
         new_file.write(what_to_write + '\n')
 
 def load_file():
-    q=1
-    print(q)
+ print('write your timetable name')
 
 print('hi, click "c" if you want to create new plan, or click "s" if you want to see existed \n ')
 
